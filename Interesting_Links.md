719ac22d629a2159bea5be3d1a0d20ee47051b93
TobiasHoesli <tobias.hoesli@hotmail.com>
2023-04-18 22:40:30 +0200

    1st try at visualizing a word tree/graph added some interesting links for semantic analysis & notes on code changes

---
+++ Interesting_Links.md
@@ -0,0 +1,14 @@
+## Document/Text Similarity Methods
+
+
+Methods:  
+https://www.baeldung.com/cs/ml-similarities-in-text
+
+Measurements:  
+https://programminghistorian.org/en/lessons/common-similarity-measures
+
+
+## Notes for Change:
+
+- Variable names should be descriptive
+- Lack of comments describing the code
--- code/01_data_evaluation.R
+++ code/01_data_evaluation.R
@@ -5,8 +5,8 @@ library(ggplot2)
 
 #Load datasets
 
-results_car <- load("results_car_selected.Rdata")
-results_energy <- load("results_energy_selected.Rdata")
+results_car <- load("data/data_processed/results_car_selected.Rdata")
+results_energy <- load("data/data_processed/results_energy_selected.Rdata")
                     
 # Count occurrences of "sustainable" in body_text column
 keyword <- "sustainable"
@@ -48,3 +48,50 @@ num_occurrences <- 4
 results_energy_selected %>%
   filter(str_count(tolower(body_text), keyword_2) == num_occurrences)
 
+
+
+
+# Word Tree/Graph --------
+
+library(tidytext)
+library(igraph)
+
+# Tokenize all the words from the article and remove stopwords
+tidy_clean_energy <- results_energy_selected %>%
+  unnest_tokens(word, body_text) %>%
+  anti_join(stop_words) %>%
+  mutate(word = str_remove_all(word, "[^[:alnum:]]")) %>%
+  filter(str_length(word) > 2)
+
+#Filter to words used more than 3 times
+word_counts <- tidy_clean_energy %>%
+  count(word, sort = TRUE) %>%
+  filter(n > 3)
+
+word_pairs <- tidy_clean_energy %>%
+  mutate(word_lead = lead(word)) %>%
+  filter(!is.na(word_lead)) %>%
+  count(word, word_lead, sort = TRUE) %>%
+  filter(n > 3)
+
+word_pairs_graph <- graph_from_data_frame(word_pairs)
+
+## Add some specifics to visualize WordTree Better
+# Set the size of the nodes
+V(word_pairs_graph)$size <- word_counts$n / max(word_counts$n) * 20 + 2
+
+# Set the size of the labels based on the size of the nodes
+V(word_pairs_graph)$label.cex <- V(word_pairs_graph)$size / max(V(word_pairs_graph)$size) * 2 + 0.5
+
+# Set the color of the nodes based on the size of the nodes
+V(word_pairs_graph)$color <- rgb(V(word_pairs_graph)$size / max(V(word_pairs_graph)$size), 0, 0)
+
+# Increase the resolution of the image by increasing the width and height parameters
+png("output/plots/word_tree.png", width = 1600, height = 1200)
+plot(word_pairs_graph,
+     edge.arrow.size = 0.5,
+     vertex.label.cex = V(word_pairs_graph)$label.cex,
+     vertex.label.color = "black",
+     vertex.label.family = "sans",
+     vertex.color = V(word_pairs_graph)$color)
+dev.off()
