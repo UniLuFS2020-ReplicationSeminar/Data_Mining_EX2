db34f41134f8befe7fe6a0cd2808181076c3fafc
Miguel Corredera Payo <co.miguel12@gmail.com>
2023-04-18 15:49:22 +0200

    Try new datasets and new keyword search

--- code/01_data_evaluation.R
+++ code/01_data_evaluation.R
@@ -5,18 +5,18 @@ library(ggplot2)
 
 #Load datasets
 
-results_car <- load("results_car.Rdata")
-results_energy <- load("results_energy.Rdata")
+results_car <- load("results_car_selected.Rdata")
+results_energy <- load("results_energy_selected.Rdata")
                     
 # Count occurrences of "better future" in body_text column
-keyword <- "better future"
+keyword <- "sustainable"
 results_energy_selected$count <- sapply(strsplit(tolower(results_energy_selected$body_text), "\\W"), function(x) sum(keyword %in% x))
 
 # Create a bar graph to visualize the results
 plot1 <- ggplot(results_energy_selected, aes(x = factor(count), fill = factor(count))) +
   geom_bar() +
-  scale_fill_discrete(name = "Count of 'better future'") +
-  labs(title = "Occurrences of 'solar energy' in Body Text",
+  scale_fill_discrete(name = "Count of 'sustainable'") +
+  labs(title = "Occurrences of 'sustainable' in body text",
        x = "Count",
        y = "Frequency")
 
